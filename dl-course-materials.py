4bb54ec2024a84a6789ab25f29eafa580dc39a78
Jodie Miu <jodiemiu@gmail.com>
2015-03-17 00:20:36 -0400

    Download function

--- dl-course-materials.py
+++ dl-course-materials.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-import urllib.request, getpass, sys, re
+import urllib.request, getpass, sys, re, urllib.error
 from bs4 import BeautifulSoup, SoupStrainer
 from urllib.parse import urljoin
+from sys import exit
 
 class HTTPAUTH:
     def __init__(self, url, username, password):
@@ -32,6 +33,7 @@ def pull_site(info):
         return urllib.request.urlopen(info.url)
     except urllib.error.HTTPError as ue:
         print(ue.reason, file=sys.stderr)
+        sys.exit(1)
 
 
 """ Keep in mind that url may not needed to be appended to the link. Depends on
@@ -48,20 +50,32 @@ def parse_for_links(url, response, filetype):
         if match:
             # if link begins with / then it is a relative link
             if match.group()[0] == '/':
-                links.push(urljoin(url, match.group()))
+                links.append(urljoin(url, match.group()))
             else:
-                links.push(match.group())
+                links.append(match.group())
     return links
 
+def download_file(download_url):
+    fn = re.compile("/(.*\.pdf$)")
+    m = fn.match(download_url)
 
+    response = urllib.request.urlopen(download_url)
+
+    name = m.group(1)
+    file = open(name, 'wb')
+    file.write(response.read())
+    file.close()
 
 def main():
     info = info_request()
 
     response = pull_site(info)
+    #file_request = input("Enter file type: ")
+    filetype = re.compile(".*\.pdf$")
+    links = parse_for_links(info.url, response, filetype)
 
-    filetype = re.compile(".+\.pdf")
-    parse_for_links(info.url, response, filetype)
+    for l in links:
+        download_file(l)
 
 
 if __name__=='__main__':
